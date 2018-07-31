41f39d31af08b09ba4b338ac69c2f19ab9f5163c
zhangzhan <zhan.zhang@realhead.cn>
2018-07-31 17:56:40 +0800

    修改解析地址逻辑

--- src/com/realcan/B2bAddressAnalysis.java
+++ src/com/realcan/B2bAddressAnalysis.java
@@ -1,5 +1,7 @@
 package com.realcan;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -15,29 +17,42 @@ public class B2bAddressAnalysis extends ExcelAnalysis {
         Set<String> tmp = new HashSet<>();
         for (Map<String, String> userMapDatum : ExcelUtil.userMapData) {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.SHIP_TO_ID.getDescription()))
-                    .append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.STREET_NAME.getDescription()))
-                    .append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.POSTALCODE.getDescription()))
-                    .append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.DESC1.getDescription()))
-                    .append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.COUNTRY.getDescription()))
-                    .append(";TRUE;TRUE;TRUE;TRUE;;")
-                    .append(userMapDatum.get(UserCellTypeEnum.NAME2.getDescription()))
-                    .append(";;rev;RealcanAddrID_")
-                    .append(userMapDatum.get(UserCellTypeEnum.SHIP_TO_ID.getDescription()))
-                    .append(";RealcanID_")
-                    .append(userMapDatum.get(UserCellTypeEnum.SOLD_TO_ID.getDescription()))
-                    .append(";")
-                    .append(userMapDatum.get(UserCellTypeEnum.COMPANYID.getDescription()))
-                    .append("-30-17_sellerOrg")
-                    .append(";CN-37;")
-                    .append(userMapDatum.get(UserCellTypeEnum.MOBILE_NUM1.getDescription()));
+            String soldToId = userMapDatum.get(UserCellTypeEnum.SOLD_TO_ID.getDescription());
+            if (StringUtils.isNotBlank(soldToId)) {
+                String shipToId = userMapDatum.get(UserCellTypeEnum.SHIP_TO_ID.getDescription());
+                stringBuilder.append(";")
+                        .append(shipToId)
+                        .append(";")
+                        .append(userMapDatum.get(UserCellTypeEnum.STREET_NAME.getDescription()))
+                        .append(";")
+                        .append(userMapDatum.get(UserCellTypeEnum.POSTALCODE.getDescription()))
+                        .append(";")
+                        .append(userMapDatum.get(UserCellTypeEnum.DESC1.getDescription()))
+                        .append(";")
+                        .append(userMapDatum.get(UserCellTypeEnum.COUNTRY.getDescription()))
+                        .append(";TRUE;TRUE;TRUE;TRUE;;")
+                        .append(userMapDatum.get(UserCellTypeEnum.NAME2.getDescription()))
+                        .append(";;rev;RealcanAddrID_")
+                        .append(shipToId)
+                        .append(";RealcanID_")
+                        .append(soldToId)
+                        .append(";")
+                        .append(userMapDatum.get(UserCellTypeEnum.COMPANYID.getDescription()))
+                        .append("-30-17_sellerOrg")
+                        .append(";CN-37;")
+                        .append(userMapDatum.get(UserCellTypeEnum.MOBILE_NUM1.getDescription()))
+                        .append(";RealcanID_")
+                        .append(soldToId);
+                if (soldToId.equals(shipToId)) {
+                    stringBuilder.append(";true");
+                } else {
+                    stringBuilder.append(";false");
+                }
 
-            tmp.add(stringBuilder.toString());
+
+                tmp.add(stringBuilder.toString());
+            }
+
         }
         result.addAll(tmp);
         result.add(">>>>>>>>>>>>>>>>>>> RealcanB2BAddress end");
